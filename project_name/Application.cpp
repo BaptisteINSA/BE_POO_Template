f77edb9122aaa9e342059416bfd91e9a3752760e
Guillaume GOURVES <guillaume.gourves@onera.Fr>
2024-11-14 08:37:00 +0100

    AJout d'une fonction LCD dans Apllication.ccpp

--- project_name/Application.cpp
+++ project_name/Application.cpp
@@ -4,7 +4,15 @@
  * @brief Fichier source de l'application
  *********************************************************************/
 #include "Application.h"
+#include <Arduino.h>
+#include <Wire.h>
+#include "rgb_lcd.h"
 
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
 
 Application::Application()
 {
@@ -21,12 +29,25 @@ Application::~Application()
 void Application::init(void)
 {
   // Code
-    ;
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+
+    lcd.setRGB(colorR, colorG, colorB);
+
+    // Print a message to the LCD.
+    lcd.print("hello, world!");
+
+    delay(1000);
 }
 
 
 void Application::run(void)
 {
-  // Code
-    ;
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(millis() / 1000);
+
+    delay(100);
 }
